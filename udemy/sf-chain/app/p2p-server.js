d12a5615dfbaa4ca6576631a7c39a847c292028b
Bryan Rodrigues <bryan@parcelz.com>
2018-07-20 05:31:49 -0400

    Setup P2p servers for syncing blockchains

--- udemy/sf-chain/app/p2p-server.js
+++ udemy/sf-chain/app/p2p-server.js
@@ -15,11 +15,43 @@ class P2pServer {
       port: P2P_PORT
     });
     server.on("connection", socket => this.connectSocket(socket));
+
+    this.connectToPeers();
+
     console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
   }
 
-  connectSocket() {
+  connectToPeers() {
+    peers.forEach(peer => {
+      const socket = new Websocket(peer);
+      socket.on("open", () => this.connectSocket(socket));
+    });
+  }
+
+  connectSocket(socket) {
     this.sockets.push(socket);
     console.log("Socket connected");
+    this.messageHandler(socket);
+    this.sendChain(socket);
+  }
+
+  messageHandler(socket) {
+    socket.on("message", message => {
+      const data = JSON.parse(message);
+      console.log("data", data);
+      this.blockchain.replaceChain(data);
+    });
+  }
+
+  sendChain(socket) {
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  syncChains() {
+    this.sockets.forEach(socket => {
+      this.sendChain(socket);
+    });
   }
 }
+
+module.exports = P2pServer;
